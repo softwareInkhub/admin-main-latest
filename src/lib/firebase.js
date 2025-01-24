8378d4eeda122809e9986ff38afd152a438f3dbd
softwareInkhub <software.inkhub@gmail.com>
2025-01-24 17:50:24 +0530

    first commit

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,16 +4,18 @@ import { getFirestore, setDoc, doc } from 'firebase/firestore';
 
 
 
+
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID,
-  };
-  
+  apiKey: "AIzaSyC0kLxRiw__uRm4AphkGbZX2h4o5Mqi29M",
+  authDomain: "inkhub-admin-v2.firebaseapp.com",
+  projectId: "inkhub-admin-v2",
+  storageBucket: "inkhub-admin-v2.firebasestorage.app",
+  messagingSenderId: "313666062878",
+  appId: "1:313666062878:web:39692b1b66db96cb08bc0f",
+  measurementId: "G-JR6GD05DHE"
+};
+
+console.log("API Key:", process.env.AUTH_DOMAIN);
 
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
